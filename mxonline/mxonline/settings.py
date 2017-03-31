ec7a7fea4395ac7e54ea7c129f8550b8b804a6d3
zrd <zrd@zrd-2.local>
2017-03-31 22:48:05 +0800

    修改后台语言 时区

--- mxonline/mxonline/settings.py
+++ mxonline/mxonline/settings.py
@@ -119,16 +119,16 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
-
+USE_TZ = False
+""" 如果不修改则是utc的时间 本地时间"""
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
