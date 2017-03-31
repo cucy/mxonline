44ab6b44bc9b151d08686fa5a8e3e06766d3b43b
cucy <292016176@qq.com>
2017-03-31 18:17:59 +0800

    将所有应用移动到apps中

---
+++ mxonline/apps/__init__.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+#-- coding: utf-8 --
+#Time: 2017/3/31 18:01
+#Author: zhourudong
--- mxonline/apps/operation/models.py
+++ mxonline/apps/operation/models.py
@@ -9,7 +9,7 @@ from courses.models import Course
 # Create your models here.
 class UserAsk(models.Model):
     name = models.CharField(max_length=20, verbose_name=u'姓名')
-    mobile = models.IntegerField(max_length=11, verbose_name=u'手机')
+    mobile = models.IntegerField( verbose_name=u'手机')
     course_name =  models.CharField(max_length=50, verbose_name=u'课程名')
     add_time = models.DateTimeField(default=datetime.now, verbose_name=u'添加时间')
 
