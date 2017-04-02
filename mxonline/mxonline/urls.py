e81c83744bc3d817f4936c883352e684b7ca3cfd
zrd <zrd@zrd-2.local>
2017-04-02 20:24:42 +0800

    login配置初步

--- mxonline/mxonline/urls.py
+++ mxonline/mxonline/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from django.views.generic import TemplateView
+from users.views import login
 import xadmin
 urlpatterns = [
     # url(r'^admin/', admin.site.urls),
     url(r'^xadmin/', xadmin.site.urls),
     url('^$', TemplateView.as_view(template_name="index.html"), name="index"),
-    url('^login/$', TemplateView.as_view(template_name="login.html"), name="login"),
+    url('^login/$', login, name="login"),
 ]
