004eb8b903d4a5a2fb0e9c591be863231bcf3812
zrd <zrd@zrd-2.local>
2017-04-02 23:10:05 +0800

    login基于类方法

--- mxonline/apps/users/views.py
+++ mxonline/apps/users/views.py
@@ -1,8 +1,9 @@
-#coding:utf8
+# coding:utf8
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.backends import ModelBackend
 from django.db.models import Q
+from django.views.generic.base import View
 from .models import UserProfile
 
 
@@ -16,8 +17,8 @@ class CustomBackend(ModelBackend):
     def authenticate(self, username=None, password=None, **kwargs):
         try:
             # 并集操作| 交集操作,
-            print(username,"------------------------")
-            user = UserProfile.objects.get(Q(username=username)|Q(email=username))
+            print(username, "------------------------")
+            user = UserProfile.objects.get(Q(username=username) | Q(email=username))
             # 传入明文进行对比
             if user.check_password(password):
                 return user
@@ -26,8 +27,11 @@ class CustomBackend(ModelBackend):
             return None
 
 
-def user_login(request):
-    if request.method == 'POST':
+class LoginView(View):
+    def get(self, request):
+        return render(request, "login.html")
+
+    def post(self, request):
         user_name = request.POST.get("username", "")
         pass_word = request.POST.get("password", "")
 
@@ -36,7 +40,5 @@ def user_login(request):
             login(request, user)
             return render(request, "index.html")
         else:
-            return render(request, "login.html",{"msg": "用户名或密码错误"})
+            return render(request, "login.html", {"msg": "用户名或密码错误"})
 
-    elif request.method == "GET":
-        return render(request, "login.html")
