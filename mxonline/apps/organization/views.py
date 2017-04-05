0c5acf265ac9ea86e0f662237e830a5a4d3b3950
zrd <zrd@zrd-2.local>
2017-04-05 22:00:26 +0800

    分页功能

--- mxonline/apps/organization/views.py
+++ mxonline/apps/organization/views.py
@@ -1,6 +1,8 @@
 # coding:utf8
 from django.shortcuts import render
 from django.views.generic import View
+from pure_pagination import Paginator, EmptyPage, PageNotAnInteger
+
 from .models import CourseOrg, CityDict
 
 # Create your views here.
@@ -17,8 +19,25 @@ class OrgView(View):
         org_nums = all_orgs.count()
         # 城市
         all_citys = CityDict.objects.all()
+
+        # 对课程机构分页
+        try:
+            page = request.GET.get('page', 1)
+        except PageNotAnInteger:
+            page = 1
+
+
+
+        # Provide Paginator with the request object for complete querystring generation
+
+        p = Paginator(all_orgs, 3, request=request)
+
+        orgs = p.page(page)
+
+
+
         return render(request, "org_list.html", {
-            "all_orgs" : all_orgs,
+            "all_orgs" : orgs,
             "all_citys" : all_citys,
             "org_nums" : org_nums,
         })
--- mxonline/mxonline/settings.py
+++ mxonline/mxonline/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
     'xadmin',
     'crispy_forms',
     'captcha',
+    'pure_pagination',
 ]
 
 AUTH_USER_MODEL = "users.UserProfile"
@@ -81,6 +82,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+
                 'django.core.context_processors.media',  # 这个设置可以在html中调用MEDIA_URL
             ],
         },
