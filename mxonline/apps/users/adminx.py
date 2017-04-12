895f6ac60e58db6366d5386a5df81e02314baa15
zrd <zrd@zrd-2.local>
2017-04-12 21:25:07 +0800

    修改 icon 图标

--- mxonline/apps/users/adminx.py
+++ mxonline/apps/users/adminx.py
@@ -12,35 +12,33 @@ from xadmin import views
 
 
 class UserProfileAdmin(UserAdmin):
-    pass
-    # 重载方法
-    # def get_form_layout(self):
-    #     # 自定义的一种显示方式
-    #     if self.org_obj:
-    #         self.form_layout = (
-    #             Main(
-    #                 Fieldset('',
-    #                          'username', 'password',
-    #                          css_class='unsort no_title'
-    #                          ),
-    #                 Fieldset(_('Personal info'),
-    #                          Row('first_name', 'last_name'),
-    #                          'email'
-    #                          ),
-    #                 Fieldset(_('Permissions'),
-    #                          'groups', 'user_permissions'
-    #                          ),
-    #                 Fieldset(_('Important dates'),
-    #                          'last_login', 'date_joined'
-    #                          ),
-    #             ),
-    #             Side(
-    #                 Fieldset(_('Status'),
-    #                          'is_active', 'is_staff', 'is_superuser',
-    #                          ),
-    #             )
-    #         )
-    #     return super(UserAdmin, self).get_form_layout()
+    model_icon = 'fa fa-user'
+    def get_form_layout(self):
+        if self.org_obj:
+            self.form_layout = (
+                Main(
+                    Fieldset('',
+                             'username', 'password',
+                             css_class='unsort no_title'
+                             ),
+                    Fieldset(_('Personal info'),
+                             Row('first_name', 'last_name'),
+                             'email'
+                             ),
+                    Fieldset(_('Permissions'),
+                             'groups', 'user_permissions'
+                             ),
+                    Fieldset(_('Important dates'),
+                             'last_login', 'date_joined'
+                             ),
+                ),
+                Side(
+                    Fieldset(_('Status'),
+                             'is_active', 'is_staff', 'is_superuser',
+                             ),
+                )
+            )
+        return super(UserAdmin, self).get_form_layout()
 
 
 
@@ -62,6 +60,11 @@ class EmailVerifyRecordAdmin(object):
     list_display = ('code', 'email', 'send_type', 'send_time',)
     search_fields = ['code', 'email', 'send_type',]
     list_filter = ('code', 'email', 'send_type', 'send_time',)
+    # 自定义图标文件
+    # 更新版本位置 extra_app/xadmin/static/xadmin/vendor/font-awesome
+    # http://fontawesome.io/  替换 css文件夹 和 fonts文件家 其他两个不需要荻花吧
+    # 替换该目录下的版本即可更新
+    model_icon = 'fa fa-address-book-o'
 
 
 class BannerAdmin(object):
